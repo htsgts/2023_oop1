e34e15e287fd396347080c5fb61a8e6bdb695c4e
htsgts <davidmoon824@naver.com>
2024-04-09 13:46:04 +0900

    C++ 실습

---
+++ Variable factor/Variable factor/Variable factor.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <typeinfo>
+#include <stdarg.h>
+
+using namespace std;
+
+template <typename T>
+void print(T arg) {
+	if (typeid(arg).name() == typeid((double)0.0).name()) {
+		printf("float");
+	}
+	else if (typeid(arg).name() == typeid((int)1).name()) {
+		printf("integer");
+	}
+	else if (typeid(arg).name() == typeid((const char*)'a').name()) {
+		printf("fmt");
+	}
+	else {
+		printf(typeid(arg).name());
+	}
+	printf(": ");
+	std::cout << arg << std::endl; 
+}
+template <typename T, typename ...Types>
+void print(T arg, Types ...args) {
+		print(arg);
+		print(args...);
+}
+int main() {
+	print("%d %d %c %f\\n", 7, 8, 'A', 5.5);
+}
